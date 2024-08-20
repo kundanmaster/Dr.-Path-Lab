785fd6aaa678defcacb3e908f1ef60a8e85bedd7
kundanmaster <kundan.masterbazar@gmail.com>
2024-08-21 02:10:28 +0530

    new changes

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -133,22 +133,15 @@ const Nav: React.FC = () => {
                         About Us
                       </Link>
                     </li>
-                    <li>
+                    {/* <li>
                       <a
                         href="/test"
                         className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                       >
                         Test
                       </a>
-                    </li>
-                    <li>
-                      <Link
-                        href="/contact"
-                        className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                      >
-                        Contact Us
-                      </Link>
-                    </li>
+                    </li> */}
+                    
                     <li>
                       <Link
                         href="/socialActivity"
@@ -171,6 +164,14 @@ const Nav: React.FC = () => {
                         className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                       >
                         Awards
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        href="/contact"
+                        className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                      >
+                        Contact Us
                       </Link>
                     </li>
                   </ul>
